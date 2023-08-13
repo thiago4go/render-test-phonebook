9ae8e02b4cb8be8f85004b655aa624ae915649be
Thiago S Shimada Ramos <113816194+thiago4go@users.noreply.github.com>
2023-08-13 21:44:22 +1000

    fix process.env.PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 const morgan = require("morgan");
 const cors = require("cors");
 app.use(express.static("build"));
